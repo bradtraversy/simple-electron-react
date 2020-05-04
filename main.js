ac85ab05ecbbccf5c57ac0b8364ffc0c16972660
Brad Traversy <traversymedia@gmail.com>
2020-05-04 13:09:54 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,83 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const url = require('url')
+
+let mainWindow
+
+let dev = false
+
+if (
+	process.env.NODE_ENV !== undefined &&
+	process.env.NODE_ENV === 'development'
+) {
+	dev = true
+}
+
+function createWindow() {
+	mainWindow = new BrowserWindow({
+		width: 1024,
+		height: 768,
+		show: false,
+		webPreferences: {
+			nodeIntegration: true,
+		},
+	})
+
+	let indexPath
+
+	if (dev && process.argv.indexOf('--noDevServer') === -1) {
+		indexPath = url.format({
+			protocol: 'http:',
+			host: 'localhost:8080',
+			pathname: 'index.html',
+			slashes: true,
+		})
+	} else {
+		indexPath = url.format({
+			protocol: 'file:',
+			pathname: path.join(__dirname, 'dist', 'index.html'),
+			slashes: true,
+		})
+	}
+
+	mainWindow.loadURL(indexPath)
+
+	// Don't show until we are ready and loaded
+	mainWindow.once('ready-to-show', () => {
+		mainWindow.show()
+
+		// Open devtools if dev
+		if (dev) {
+			const {
+				default: installExtension,
+				REACT_DEVELOPER_TOOLS,
+			} = require('electron-devtools-installer')
+
+			installExtension(REACT_DEVELOPER_TOOLS).catch((err) =>
+				console.log('Error loading React DevTools: ', err)
+			)
+			mainWindow.webContents.openDevTools()
+		}
+	})
+
+	mainWindow.on('closed', function () {
+		mainWindow = null
+	})
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit()
+	}
+})
+
+app.on('activate', () => {
+	if (mainWindow === null) {
+		createWindow()
+	}
+})
+
+// Stop error
+app.allowRendererProcessReuse = true
