af5f655bc28ddb1eb74251af70cab866141382a1
James Sizeland <jamessizeland@gmail.com>
2020-12-28 22:32:07 +0000

    adds colour coding to priority indicators in LogItem.js

--- src/components/LogItem.js
+++ src/components/LogItem.js
@@ -1,11 +1,32 @@
 // rafce = snippet shortcut for this code structure
 import React from "react";
 import Button from "react-bootstrap/Button";
+import Badge from "react-bootstrap/Badge";
 
 const LogItem = ({ log: { _id, priority, user, text, created } }) => {
+  /*******************************************************
+  Private Data
+  *******************************************************/
+  const setVariant = () => {
+    switch (priority) {
+      case "high":
+        return "danger";
+      case "medium":
+        return "warning";
+      default:
+        return "success";
+    }
+  };
+  /*******************************************************
+  Output
+  *******************************************************/
   return (
     <tr>
-      <td>{priority}</td>
+      <td>
+        <Badge variant={setVariant()} className="p-2">
+          {priority.charAt(0).toUpperCase() + priority.slice(1)}
+        </Badge>
+      </td>
       <td>{text}</td>
       <td>{user}</td>
       <td>{created}</td>
